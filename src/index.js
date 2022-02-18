3b76f366398434a5ee98abc2132c002dc3f53a65
eumali22 <97612829+eumali22@users.noreply.github.com>
2022-02-18 14:56:44 +0800

    added switch to main rendered components

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import Example from './hook-example.js';
 
 /*****************/
 
+// manual switch to select which app to display
+const key = 'hook-example';
+var componentToShow;
+
+switch (key) {
+  case 'hook-example':
+    componentToShow = <Example />
+    break;
+
+  default:
+    componentToShow = <Example />
+    break;
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <Example />
+    {componentToShow}
   </React.StrictMode>,
   document.getElementById('root')
 );
-
