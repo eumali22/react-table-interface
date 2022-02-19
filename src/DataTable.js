88aa5b0ba07b1a0d7fc934ca7bc0f8a9a5228aec
eumali22 <97612829+eumali22@users.noreply.github.com>
2022-02-19 19:37:33 +0800

    added componentDidMount() call

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,40 +1,80 @@
 import React from 'react';
 import './DataTable.css';
 
+/**
+ * Component that represents a table. It is stateful.
+ * @extends React.Component
+ * State properties:
+ *  - tableData: contains the table data to be rendered.
+ */
 class DataTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            dataArray: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            tableData: [],
+            dataIsLoaded: false,
         }
     }
 
+    componentDidMount() {
+        // TO DO fetch code here
+        
+        // simulate loading time. 1000 ms
+        setTimeout(() => {
+            this.setState({
+                tableData: [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]],
+                dataIsLoaded: true,
+            });
+        }, 1000);
+    }
+
     render() {
+        const {dataIsLoaded, tableData} = this.state;
+        if (!dataIsLoaded) {
+            return (
+                <div>Loading, please wait... </div>
+            );
+        }
+
+        // create the row components
+        const rowComponents = tableData.map((row, idx) => {
+            return (
+                <DataTableRow key={idx} cells={tableData[idx]} />
+            );
+        });
+
         return (
-            <div class="dtable">
-                <DataTableRow data={this.state.dataArray[0]} />
-                <DataTableRow data={this.state.dataArray[1]} />
-                <DataTableRow data={this.state.dataArray[2]} />
-            </div>
+            <div className="dtable">{rowComponents}</div>
         );
     }
 }
 
-
+/**
+ * A row for a DataTable. Functional component.
+ * @param {*} props an object; should have property called cells.
+ * @returns a DataTableRow component containing DataTableColumn components.
+ */
 function DataTableRow(props) {
+    // create the cells
+    const columnComponents = props.cells.map((col, idx) => {
+        return (
+            <DataTableCell key={idx} data={props.cells[idx]} />
+        );
+    });
+
     return (
-        <div class="dtable-row">
-            <DataTableColumn data={props.data[0]} />
-            <DataTableColumn data={props.data[1]} />
-            <DataTableColumn data={props.data[2]} />
-        </div>
+        <div className="dtable-row">{columnComponents}</div>
     );
 }
 
-
-function DataTableColumn(props) {
+/**
+ * A single cell for a DataTable. Functional component.
+ * @param {*} props 
+ * @returns a DataTableCell component.
+ */
+function DataTableCell(props) {
     return (
-        <div class="dtable-col">{props.data}</div>
+        <div className="dtable-cell">{props.data}</div>
     );
 }
 
